9d2e090002c487ce7ae1a7bb7ad5d401956b600f
iahmedelbayaa <elbayaaahmed845@gmail.com>
2023-12-11 03:11:23 +0200

    init-file-structure

---
+++ src/db/dbConnection.ts
@@ -0,0 +1,15 @@
+import pool from './pool';
+
+const query = (queryText: string, queryParams: any) => {
+    return new Promise((resolve, reject) => {
+        pool.query(queryText, queryParams).then(res => {
+            resolve(res);
+        })
+        .catch(err => {
+            reject(err);
+        });
+    });
+};
+
+export default query;
+
